c903670870caa706ab173ab8c622c96659c9c5ea
sishii0418 <s.ishii@sms.ed.ac.uk>
2022-11-28 20:26:22 +0000

    now possible to move pieces, if it's your turn

--- engine.py
+++ engine.py
@@ -28,228 +28,234 @@ class GameState:
             return False
 
     # Returns possible location for a specified piece to move
-    def move(self, piece_loc: tuple[int, int]) -> Optional[set[tuple[int, int]]]:
+    def towhere(self, piece_loc: tuple[int, int], whites_turn: bool) -> set[tuple[int, int]]:
         x, y = piece_loc
+        rt = set()
         def zero2seven(xy: tuple[int, int]) -> bool:
             return 0 <= xy[0] and xy[0] <= 7 and 0 <= xy[1] and xy[1] <= 7
         if self.board[x][y] in ["bK", "wK"]:
             # FIXME: 移動先でチェックメイトされるようなマスには移動できない
-            rt = set(filter(zero2seven, [(x+1, y+1), (x+1, y), (x+1, y-1), (x, y+1), (x, y), (x, y-1),(x-1, y+1), (x-1, y), (x-1, y-1),]))
-            return set(filter(lambda x: not self.isMyPiece(piece_loc, x), rt))
+            if (whites_turn and self.board[x][y] == "wK") or (not(whites_turn) and self.board[x][y] == "bK"):
+                rt = rt.union(set(filter(zero2seven, [(x+1, y+1), (x+1, y), (x+1, y-1), (x, y+1), (x, y), (x, y-1),(x-1, y+1), (x-1, y), (x-1, y-1),])))
+                return set(filter(lambda x: not self.isMyPiece(piece_loc, x), rt))
         elif self.board[x][y] in ["bQ", "wQ"]:
-            rt = set([])
-            # 左
-            while 0 < x:
-                if self.board[x-1][y] != None:
-                    if self.isMyPiece(piece_loc, (x-1, y)):
-                        break
-                    else:
-                        rt.add((x-1, y))
-                        break
-                rt.add((x-1, y))
-                x -= 1
-            # 右
-            x, y = piece_loc
-            while x < 7:
-                if self.board[x+1][y] != None:
-                    if self.isMyPiece(piece_loc, (x+1, y)):
-                        break
-                    else:
-                        rt.add((x+1, y))
-                        break
-                rt.add((x+1, y))
-                x += 1
-            # 下
-            x, y = piece_loc
-            while y < 7:
-                if self.board[x][y+1] != None:
-                    if self.isMyPiece(piece_loc, (x, y+1)):
-                        break
-                    else:
-                        rt.add((x, y+1))
-                        break
-                rt.add((x, y+1))
-                y += 1
-            # 上
-            x, y = piece_loc
-            while 0 < y:
-                if self.board[x][y-1] != None:
-                    if self.isMyPiece(piece_loc, (x, y-1)):
-                        break
-                    else:
-                        rt.add((x, y-1))
-                        break
-                rt.add((x, y-1))
-                y -= 1
-            # 斜め
-            # 左上
-            while 0 < x and 0 < y:
-                if self.board[x-1][y-1] != None:
-                    if self.isMyPiece(piece_loc, (x-1, y-1)):
-                        break
-                    else:
-                        rt.add((x-1, y-1))
-                        break
-                rt.add((x-1, y-1))
-                x -= 1
-                y -= 1
-            # 右下
-            x, y = piece_loc
-            while x < 7 and y < 7:
-                if self.board[x+1][y+1] != None:
-                    if self.isMyPiece(piece_loc, (x+1, y+1)):
-                        break
-                    else:
-                        rt.add((x+1, y+1))
-                        break
-                rt.add((x+1, y+1))
-                x += 1
-                y += 1
-            # 右上
-            x, y = piece_loc
-            while 0 < x and y < 7:
-                if self.board[x-1][y+1] != None:
-                    if self.isMyPiece(piece_loc, (x-1, y+1)):
-                        break
-                    else:
-                        rt.add((x-1, y+1))
-                        break
-                rt.add((x-1, y+1))
-                x -= 1
-                y += 1
-            # 左下
-            x, y = piece_loc
-            while x < 7 and 0 < y:
-                if self.board[x+1][y-1] != None:
-                    if self.isMyPiece(piece_loc, (x+1, y-1)):
-                        break
-                    else:
-                        rt.add((x+1, y-1))
-                        break
-                rt.add((x+1, y-1))
-                x += 1
-                y -= 1
-            return rt
+            if (whites_turn and self.board[x][y] == "wQ") or (not(whites_turn) and self.board[x][y] == "bQ"):
+                # 左
+                while 0 < x:
+                    if self.board[x-1][y] != None:
+                        if self.isMyPiece(piece_loc, (x-1, y)):
+                            break
+                        else:
+                            rt.add((x-1, y))
+                            break
+                    rt.add((x-1, y))
+                    x -= 1
+                # 右
+                x, y = piece_loc
+                while x < 7:
+                    if self.board[x+1][y] != None:
+                        if self.isMyPiece(piece_loc, (x+1, y)):
+                            break
+                        else:
+                            rt.add((x+1, y))
+                            break
+                    rt.add((x+1, y))
+                    x += 1
+                # 下
+                x, y = piece_loc
+                while y < 7:
+                    if self.board[x][y+1] != None:
+                        if self.isMyPiece(piece_loc, (x, y+1)):
+                            break
+                        else:
+                            rt.add((x, y+1))
+                            break
+                    rt.add((x, y+1))
+                    y += 1
+                # 上
+                x, y = piece_loc
+                while 0 < y:
+                    if self.board[x][y-1] != None:
+                        if self.isMyPiece(piece_loc, (x, y-1)):
+                            break
+                        else:
+                            rt.add((x, y-1))
+                            break
+                    rt.add((x, y-1))
+                    y -= 1
+                # 斜め
+                # 左上
+                while 0 < x and 0 < y:
+                    if self.board[x-1][y-1] != None:
+                        if self.isMyPiece(piece_loc, (x-1, y-1)):
+                            break
+                        else:
+                            rt.add((x-1, y-1))
+                            break
+                    rt.add((x-1, y-1))
+                    x -= 1
+                    y -= 1
+                # 右下
+                x, y = piece_loc
+                while x < 7 and y < 7:
+                    if self.board[x+1][y+1] != None:
+                        if self.isMyPiece(piece_loc, (x+1, y+1)):
+                            break
+                        else:
+                            rt.add((x+1, y+1))
+                            break
+                    rt.add((x+1, y+1))
+                    x += 1
+                    y += 1
+                # 右上
+                x, y = piece_loc
+                while 0 < x and y < 7:
+                    if self.board[x-1][y+1] != None:
+                        if self.isMyPiece(piece_loc, (x-1, y+1)):
+                            break
+                        else:
+                            rt.add((x-1, y+1))
+                            break
+                    rt.add((x-1, y+1))
+                    x -= 1
+                    y += 1
+                # 左下
+                x, y = piece_loc
+                while x < 7 and 0 < y:
+                    if self.board[x+1][y-1] != None:
+                        if self.isMyPiece(piece_loc, (x+1, y-1)):
+                            break
+                        else:
+                            rt.add((x+1, y-1))
+                            break
+                    rt.add((x+1, y-1))
+                    x += 1
+                    y -= 1
+                return rt
         elif self.board[x][y] in ["bR", "wR"]:
-            rt = set([])
-            # 左
-            while 0 < x:
-                if self.board[x-1][y] != None:
-                    if self.isMyPiece(piece_loc, (x-1, y)):
-                        break
-                    else:
-                        rt.add((x-1, y))
-                        break
-                rt.add((x-1, y))
-                x -= 1
-            # 右
-            x, y = piece_loc
-            while x < 7:
-                if self.board[x+1][y] != None:
-                    if self.isMyPiece(piece_loc, (x+1, y)):
-                        break
-                    else:
-                        rt.add((x+1, y))
-                        break
-                rt.add((x+1, y))
-                x += 1
-            # 下
-            x, y = piece_loc
-            while y < 7:
-                if self.board[x][y+1] != None:
-                    if self.isMyPiece(piece_loc, (x, y+1)):
-                        break
-                    else:
-                        rt.add((x, y+1))
-                        break
-                rt.add((x, y+1))
-                y += 1
-            # 上
-            x, y = piece_loc
-            while 0 < y:
-                if self.board[x][y-1] != None:
-                    if self.isMyPiece(piece_loc, (x, y-1)):
-                        break
-                    else:
-                        rt.add((x, y-1))
-                        break
-                rt.add((x, y-1))
-                y -= 1
-            return rt
+            if (whites_turn and self.board[x][y] == "wR") or (not(whites_turn) and self.board[x][y] == "bR"):
+                # 左
+                while 0 < x:
+                    if self.board[x-1][y] != None:
+                        if self.isMyPiece(piece_loc, (x-1, y)):
+                            break
+                        else:
+                            rt.add((x-1, y))
+                            break
+                    rt.add((x-1, y))
+                    x -= 1
+                # 右
+                x, y = piece_loc
+                while x < 7:
+                    if self.board[x+1][y] != None:
+                        if self.isMyPiece(piece_loc, (x+1, y)):
+                            break
+                        else:
+                            rt.add((x+1, y))
+                            break
+                    rt.add((x+1, y))
+                    x += 1
+                # 下
+                x, y = piece_loc
+                while y < 7:
+                    if self.board[x][y+1] != None:
+                        if self.isMyPiece(piece_loc, (x, y+1)):
+                            break
+                        else:
+                            rt.add((x, y+1))
+                            break
+                    rt.add((x, y+1))
+                    y += 1
+                # 上
+                x, y = piece_loc
+                while 0 < y:
+                    if self.board[x][y-1] != None:
+                        if self.isMyPiece(piece_loc, (x, y-1)):
+                            break
+                        else:
+                            rt.add((x, y-1))
+                            break
+                    rt.add((x, y-1))
+                    y -= 1
+                return rt
         elif self.board[x][y] in ["bB", "wB"]:
-            rt = set([])
-            # 斜め
-            # 左上
-            while 0 < x and 0 < y:
-                if self.board[x-1][y-1] != None:
-                    if self.isMyPiece(piece_loc, (x-1, y-1)):
-                        break
-                    else:
-                        rt.add((x-1, y-1))
-                        break
-                rt.add((x-1, y-1))
-                x -= 1
-                y -= 1
-            # 右下
-            x, y = piece_loc
-            while x < 7 and y < 7:
-                if self.board[x+1][y+1] != None:
-                    if self.isMyPiece(piece_loc, (x+1, y+1)):
-                        break
-                    else:
-                        rt.add((x+1, y+1))
-                        break
-                rt.add((x+1, y+1))
-                x += 1
-                y += 1
-            # 右上
-            x, y = piece_loc
-            while 0 < x and y < 7:
-                if self.board[x-1][y+1] != None:
-                    if self.isMyPiece(piece_loc, (x-1, y+1)):
-                        break
-                    else:
-                        rt.add((x-1, y+1))
-                        break
-                rt.add((x-1, y+1))
-                x -= 1
-                y += 1
-            # 左下
-            x, y = piece_loc
-            while x < 7 and 0 < y:
-                if self.board[x+1][y-1] != None:
-                    if self.isMyPiece(piece_loc, (x+1, y-1)):
-                        break
-                    else:
-                        rt.add((x+1, y-1))
-                        break
-                rt.add((x+1, y-1))
-                x += 1
-                y -= 1
-            return rt
+            if (whites_turn and self.board[x][y] == "wB") or (not(whites_turn) and self.board[x][y] == "bB"):
+                # 斜め
+                # 左上
+                while 0 < x and 0 < y:
+                    if self.board[x-1][y-1] != None:
+                        if self.isMyPiece(piece_loc, (x-1, y-1)):
+                            break
+                        else:
+                            rt.add((x-1, y-1))
+                            break
+                    rt.add((x-1, y-1))
+                    x -= 1
+                    y -= 1
+                # 右下
+                x, y = piece_loc
+                while x < 7 and y < 7:
+                    if self.board[x+1][y+1] != None:
+                        if self.isMyPiece(piece_loc, (x+1, y+1)):
+                            break
+                        else:
+                            rt.add((x+1, y+1))
+                            break
+                    rt.add((x+1, y+1))
+                    x += 1
+                    y += 1
+                # 右上
+                x, y = piece_loc
+                while 0 < x and y < 7:
+                    if self.board[x-1][y+1] != None:
+                        if self.isMyPiece(piece_loc, (x-1, y+1)):
+                            break
+                        else:
+                            rt.add((x-1, y+1))
+                            break
+                    rt.add((x-1, y+1))
+                    x -= 1
+                    y += 1
+                # 左下
+                x, y = piece_loc
+                while x < 7 and 0 < y:
+                    if self.board[x+1][y-1] != None:
+                        if self.isMyPiece(piece_loc, (x+1, y-1)):
+                            break
+                        else:
+                            rt.add((x+1, y-1))
+                            break
+                    rt.add((x+1, y-1))
+                    x += 1
+                    y -= 1
+                return rt
         elif self.board[x][y] in ["bN", "wN"]:
-            rt = set(filter(zero2seven, {(x+2, y+1), (x+2, y-1), (x+1, y+2), (x+1, y-2), (x-1, y+2), (x-1, y-2), (x-2, y+1), (x-2, y-1)}))
-            return set(filter(lambda x: not self.isMyPiece(piece_loc, x), rt))
+            if (whites_turn and self.board[x][y] == "wN") or (not(whites_turn) and self.board[x][y] == "bN"):
+                rt = set(filter(zero2seven, {(x+2, y+1), (x+2, y-1), (x+1, y+2), (x+1, y-2), (x-1, y+2), (x-1, y-2), (x-2, y+1), (x-2, y-1)}))
+                rt = set(filter(lambda x: not self.isMyPiece(piece_loc, x), rt))
+                if rt is None:
+                    return set()
+                else:
+                    return rt
         elif self.board[x][y] in ["bP", "wP"]:
-            rt = set([])
-            if self.board[x][y] == "bP":
+            if self.board[x][y] == "bP" and not(whites_turn):
                 if x == 1:
-                    rt = set(filter(lambda x: self.coor(x) is None, {(x+1, y), (x+2, y)}))
+                    rt = rt.union(set(filter(lambda x: self.coor(x) is None, {(x+1, y), (x+2, y)})))
                 else:
                     if x+1 <= 7:
-                        rt = set(filter(lambda x: self.coor(x) is None, {(x+1, y)}))
+                        rt = rt.union(set(filter(lambda x: self.coor(x) is None, {(x+1, y)})))
                 if zero2seven((x+1, y+1)) and self.coor((x+1, y+1)) != None:
                     if not self.isMyPiece(piece_loc, (x+1, y+1)):
                         rt.add((x+1, y+1))
                 if zero2seven((x+1, y-1)) and self.coor((x+1, y-1)) != None:
                     if not self.isMyPiece(piece_loc, (x+1, y-1)):
                         rt.add((x+1, y-1))
-            else: # self.board[x][y] == "wP"
+            elif self.board[x][y] == "wP" and whites_turn:
                 if x == 6:
-                    rt = set(filter(lambda x: self.coor(x) is None, {(x-1, y), (x-2, y)}))
+                    rt = rt.union(set(filter(lambda x: self.coor(x) is None, {(x-1, y), (x-2, y)})))
                 else:
                     if 0 <= x-1:
-                        rt = set(filter(lambda x: self.coor(x) is None, {(x-1, y)}))
+                        rt = rt.union(set(filter(lambda x: self.coor(x) is None, {(x-1, y)})))
                 if zero2seven((x-1, y+1)) and self.coor((x-1, y+1)) != None:
                     if not self.isMyPiece(piece_loc, (x-1, y+1)):
                         rt.add((x-1, y+1))
@@ -257,3 +263,7 @@ class GameState:
                     if not self.isMyPiece(piece_loc, (x-1, y-1)):
                         rt.add((x-1, y-1))
             return rt
+    
+    def move(self, piece_loc: tuple[int, int], dest: tuple[int, int]) -> None:
+        self.board[dest[0]][dest[1]] = self.coor(piece_loc)
+        self.board[piece_loc[0]][piece_loc[1]] = None
--- main.py
+++ main.py
@@ -33,8 +33,10 @@ pieces_dict: dict[piece, pygame.surface.Surface] = {"bB": bB, "bK": bK, "bN": bN
 screen = pygame.display.set_mode(size)
 
 
-selected_cell: tuple[Optional[int], Optional[int]] = (None, None)
+selected_cell: Optional[tuple[int, int]] = None
 possible_destinations: Optional[set[tuple[int, int]]] = None
+selected_piece: Optional[tuple[int, int]] = None
+whites_turn: bool = True
 
 def switch(x: tuple[int, int]) -> tuple[int, int]:
     return (x[1], x[0])
@@ -56,7 +58,21 @@ while True:
     if pygame.mouse.get_pressed()[0]:
         cursor_pos: tuple[int, int] = pygame.mouse.get_pos()
         selected_cell = (cursor_pos[0]//60, cursor_pos[1]//60)
-        possible_destinations = gs.move(switch(selected_cell))
+
+        if possible_destinations is None: # 初回選択
+            selected_piece = switch(selected_cell)
+            possible_destinations = gs.towhere(selected_piece, whites_turn)
+        else: # ピース選択中に別のセルをクリック
+            if switch(selected_cell) in possible_destinations: # 移動可能
+                gs.move(selected_piece, switch(selected_cell))
+                selected_piece = None
+                possible_destinations = None
+                selected_cell = None
+                whites_turn = not whites_turn
+            # elif gs.coor(switch(selected_cell)) is not None: # 移動不可
+            else:
+                selected_piece = switch(selected_cell)
+                possible_destinations = gs.towhere(selected_piece, whites_turn)
 
     for i in range(8):
         for j in range(8):
